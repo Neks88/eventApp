3d2ea216f3995ad312e8dd8ff344448925a4d042
neks <neks.92@live.com>
2018-10-14 21:29:42 +0200

    pocetak

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const  Navbar=()=> {
+  return (
+    <nav className="nav-wrapped grey darken-3">
+            <div className="container">
+                <Link to="/" className="brand-logo" >Mario Plan</Link>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
